7a001b16c27e283bb15963fc358fd65b82519638
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:10:45 +0800

    init project

---
+++ ios/swift/KFileSyncKit/KFileSyncKit/KFileSyncKit.h
@@ -0,0 +1,19 @@
+//
+//  KFileSyncKit.h
+//  KFileSyncKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KFileSyncKit.
+FOUNDATION_EXPORT double KFileSyncKitVersionNumber;
+
+//! Project version string for KFileSyncKit.
+FOUNDATION_EXPORT const unsigned char KFileSyncKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KFileSyncKit/PublicHeader.h>
+
+
